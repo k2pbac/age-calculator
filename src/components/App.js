83446c94ae66dcf774b4caea6fc8ca2a5e239cb4
k2pbac <krisbachan123@hotmail.com>
2024-06-14 22:03:43 -0400

    finish basic styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,30 @@
 import "../css/App.css";
 import DateInput from "../components/DateInput";
 import { Form, Image } from "react-bootstrap";
+import DateView from "./DateView";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Form className="inputs poppins-bold">
-          <DateInput label="Day" placeholder="DD" />
-          <DateInput label="Month" placeholder="MM" />
-          <DateInput label="Year" placeholder="YYYY" />
-        </Form>
-        <div className="arrow-container">
-          <hr></hr>
-          <Image
-            className="arrow"
-            src="/images/icon-arrow.svg"
-            alt="arrow"
-            roundedCircle
-          />
-        </div>
-      </header>
+      <Form className="inputs poppins-bold">
+        <DateInput label="Day" placeholder="DD" />
+        <DateInput label="Month" placeholder="MM" />
+        <DateInput label="Year" placeholder="YYYY" />
+      </Form>
+      <div className="arrow-container">
+        <hr></hr>
+        <Image
+          className="arrow"
+          src="/images/icon-arrow.svg"
+          alt="arrow"
+          roundedCircle
+        />
+      </div>
+      <div className="date-views">
+        <DateView number={-1} label="years" />
+        <DateView number={-1} label="months" />
+        <DateView number={-1} label="days" />
+      </div>
     </div>
   );
 }
