900520a6f73832d4c8e3b589d26a08ac3eb61ba1
k2pbac <krisbachan123@hotmail.com>
2024-06-15 16:26:02 -0400

    fix input

--- src/components/DateView.js
+++ src/components/DateView.js
@@ -1,16 +1,13 @@
 import "../css/DateView.css";
-import { Form } from "react-bootstrap";
 
 function DateView({ label, number }) {
   return (
-    <Form.Group className="mb-3" controlId={label}>
-      <Form.Control
-        className="poppins-bold"
-        value={number != -1 ? number : "- -"}
-        type="text"
-      />
-      <Form.Label className="poppins-extrabold-italic">{label}</Form.Label>
-    </Form.Group>
+    <div>
+      <p className="date-value poppins-bold">
+        {number !== -1 ? number : "- -"}
+      </p>
+      <p className="date-name poppins-extrabold-italic">{label} </p>
+    </div>
   );
 }
 
