8199f100af74e80fabfd6568ffb28f74c31e5261
k2pbac <krisbachan123@hotmail.com>
2024-06-16 11:23:14 -0400

    finish empty inputs

--- src/components/App.js
+++ src/components/App.js
@@ -9,22 +9,39 @@ function App() {
   const [day, setDay] = useState(-1);
   const [month, setMonth] = useState(-1);
   const [year, setYear] = useState(-1);
-
+  const [validated, setValidated] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { days, months, years } = calculateAge(
-      e.target[0].value,
-      e.target[1].value,
-      e.target[2].value
-    );
-    setDay(days);
-    setMonth(months);
-    setYear(years);
+    const day = e.target[0].value;
+    const month = e.target[1].value;
+    const year = e.target[2].value;
+
+    if (day !== "" && day >= 1 && day <= 31) {
+      if (month !== "" && month >= 1 && month <= 12) {
+        if (
+          year !== "" &&
+          year <= new Date().getFullYear() &&
+          new Date(year, month, day) < new Date()
+        ) {
+          const { days, months, years } = calculateAge(
+            e.target[0].value,
+            e.target[1].value,
+            e.target[2].value
+          );
+          setDay(days);
+          setMonth(months);
+          setYear(years);
+        }
+      }
+    }
+    setValidated(true);
   };
 
   return (
     <div className="App">
       <Form
+        noValidate
+        validated={validated}
         id="input-form"
         className="inputs poppins-bold"
         onSubmit={handleSubmit}
--- src/components/DateInput.js
+++ src/components/DateInput.js
@@ -5,7 +5,10 @@ function DateInput({ label, placeholder }) {
   return (
     <Form.Group className="mb-3" controlId={label}>
       <Form.Label>{label.toUpperCase()}</Form.Label>
-      <Form.Control type="text" placeholder={placeholder} />
+      <Form.Control required type="text" placeholder={placeholder} />
+      <Form.Control.Feedback type="invalid">
+        This field is required
+      </Form.Control.Feedback>
     </Form.Group>
   );
 }
